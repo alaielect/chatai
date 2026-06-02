1bf59d75a559e53a7d53954b734dea3e53bfd7ab
alaielect <alaielect@gmail.com>
2026-06-02 22:12:33 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,25 +2,17 @@
 
 set -e
 
-# دانلود مدل در اولین اجرا
 mkdir -p models
 
 if [ ! -f models/SmolLM2-135M-Instruct.Q8_0.gguf ]; then
+    echo "Downloading model..."
     wget -O models/SmolLM2-135M-Instruct.Q8_0.gguf \
     "https://huggingface.co/unsloth/SmolLM2-135M-Instruct-GGUF/resolve/main/SmolLM2-135M-Instruct.Q8_0.gguf"
 fi
 
-# دانلود llama.cpp
-if [ ! -d llama.cpp ]; then
-    git clone https://github.com/ggml-org/llama.cpp.git
-fi
+echo "Starting llama-server..."
 
-cd llama.cpp
-
-cmake -B build
-cmake --build build -j
-
-./build/bin/llama-server \
-  -m ../models/SmolLM2-135M-Instruct.Q8_0.gguf \
+./llama.cpp/build/bin/llama-server \
+  -m models/SmolLM2-135M-Instruct.Q8_0.gguf \
   --host 0.0.0.0 \
-  --port $PORT
+  --port ${PORT:-8080}
